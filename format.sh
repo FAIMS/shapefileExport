36e489413ba84350ed18eff915fceb704e0c7696
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2014-11-27 18:18:37 +1100

    Update format.sh

--- format.sh
+++ format.sh
@@ -3,5 +3,6 @@
 # Load RVM into a shell session *as a function*
 export PATH="$HOME/.rbenv/bin:$PATH" 
 eval "$(rbenv init -)"
+cd $2
 ruby string_formatter_tester.rb $1/db.sqlite3 shape.format > $3/shape.out
 
