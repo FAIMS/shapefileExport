cc4f9443d92515c2e92e13c1430c64cd6752c1aa
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2014-11-27 18:22:53 +1100

    Update format.sh

--- format.sh
+++ format.sh
@@ -6,5 +6,5 @@ eval "$(rbenv init -)"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
 cd $2
-ruby $DIR/string_formatter_tester.rb $1/db.sqlite3 shape.format > $3/shape.out
+ruby $DIR/string_formatter_tester.rb $1/db.sqlite3 $DIR/shape.format > $3/shape.out
 
