e1794c37468123f544daf0ee525ad5e781d204f3
Brian Ballsun-Stanton <brian@fedarch.org>
2015-03-12 21:04:33 +1100

    undo

--- shapefile.py
+++ shapefile.py
@@ -40,66 +40,6 @@ import imghdr
 from collections import defaultdict
 
 
-
-class UTF8Recoder:
-    """
-    Iterator that reads an encoded stream and reencodes the input to UTF-8
-    """
-    def __init__(self, f, encoding):
-        self.reader = codecs.getreader(encoding)(f)
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        return self.reader.next().encode("utf-8")
-
-class UnicodeReader:
-    """
-    A CSV reader which will iterate over lines in the CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
-        self.reader = csv.reader(f, dialect=dialect, **kwds)
-
-    def next(self):
-        row = self.reader.next()
-        return [unicode(s, "utf-8") for s in row]
-
-    def __iter__(self):
-        return self
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-
-
 print sys.argv
 
 def dict_factory(cursor, row):
@@ -366,7 +306,7 @@ for at in importCon.execute("select aenttypename from aenttype"):
 
 
 	files.append("Entity-%s.csv" % (aenttypename))
-	csv_writer = UnicodeWriter(open(exportDir+"Entity-%s.csv" % (aenttypename), "wb+"))
+	csv_writer = csv.writer(open(exportDir+"Entity-%s.csv" % (aenttypename), "wb+"))
 	csv_writer.writerow([i[0] for i in cursor.description]) # write headers
 	csv_writer.writerows(cursor)
 
@@ -382,7 +322,7 @@ relncursor = importCon.cursor()
 for relntypeid, relntypename in relntypecursor.execute(relntypequery): 
 	relncursor.execute(relnquery, [relntypename])
 	files.append("Relationship-%s.csv" % (clean(relntypename)))
-	csv_writer = UnicodeWriter(open(exportDir+"Relationship-%s.csv" % (clean(relntypename)), "wb+"))
+	csv_writer = csv.writer(open(exportDir+"Relationship-%s.csv" % (clean(relntypename)), "wb+"))
 	csv_writer.writerow([i[0] for i in relncursor.description]) # write headers
 	csv_writer.writerows(relncursor)
 
