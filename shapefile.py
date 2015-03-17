d90a5ed3519f421a20d899630a038c7570cf6b11
Brian Ballsun-Stanton <brian@fedarch.org>
2015-03-18 10:27:35 +1100

    Rock Observation Formatstring

--- shapefile.py
+++ shapefile.py
@@ -280,7 +280,7 @@ for line in f.readlines():
 	if (len(out) ==4):		
 		update = "update %s set %s = ? where uuid = %s;" % (clean(out[1]), clean(out[2]), out[0])
 		data = (unicode(out[3].replace("\\n","\n").replace("'","''"), errors="replace"),)
-		print update, data
+		# print update, data
 		exportCon.execute(update, data)
 
 
