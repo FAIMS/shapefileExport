8dd085573499804c30b6afccf22a9e25ecf60f8d
Brian Ballsun-Stanton <brian@fedarch.org>
2015-11-19 13:31:32 +1100

    timing

--- export.sh
+++ export.sh
@@ -11,10 +11,10 @@ json=`python -mjson.tool $2`
 # export database to csv using json inputs and pass output into export file inside download directory
 
 echo -e "\nExport Started: $(date)" > $4
-python shapefile.py $1 $3 $2 > /tmp/bar 2> /tmp/foo
+command time -o /tmp/timing python shapefile.py $1 $3 $2 > /tmp/bar 2> /tmp/foo
 echo -e "\nExport Finished: $(date)\n" >> $4
 
-cat /tmp/tming >> $4
+cat /tmp/timing >> $4
 
 echo "**Your data have been prepared for export.**
 
