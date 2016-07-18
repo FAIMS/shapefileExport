1bd1da24a65cb0e6ddf939d6423d333038bc30e8
Brian Ballsun-Stanton <brian@fedarch.org>
2016-07-18 11:55:45 +1000

    debug

--- shapefile.py
+++ shapefile.py
@@ -444,7 +444,7 @@ relntypecursor = importCon.cursor()
 relncursor = importCon.cursor()
 for relntypeid, relntypename in relntypecursor.execute(relntypequery): 
     relncursor.execute(relnquery, [relntypename])
-
+    print relnquery, relntypename
     exportCon.execute("CREATE TABLE %s (parentuuid TEXT, childuuid TEXT, participatesverb TEXT);" % (clean(relntypename)))
     for i in relncursor:
         exportCon.execute("INSERT INTO %s VALUES(?,?,?)" % (clean(relntypename)), i)
