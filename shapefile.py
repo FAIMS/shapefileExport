3ebb4f949adde96c9265c9751f34031436494368
Brian Ballsun-Stanton <brian@fedarch.org>
2017-06-23 12:01:25 +1000

    Equality in keyval

--- shapefile.py
+++ shapefile.py
@@ -254,8 +254,9 @@ f = open(arch16nFile, 'r')
 for line in f:
     if "=" in line:
         keyvalPrep = line.replace("\n","").replace("\r","").decode("utf-8").split('=')
+        keyval    = []
         keyval[0] = '{'+keyvalPrep[0]+'}'
-        keyval[1] = ' '.join(keyvalPrep[1:])
+        keyval[1] = ' '.join(keyvalPrep[1:])    
         exportCon.execute("replace into keyval(key, val) VALUES(?, ?)", keyval)
 f.close()
 
