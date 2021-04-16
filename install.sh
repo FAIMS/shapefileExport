7916171310680d55ca6f5ad42ec35da733ae8f02
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2021-04-16 11:57:57 +1000

    setting local to 2.5.6

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ sudo apt-get install spatialite-bin libimage-exiftool-perl libspatialite-dev pyt
 
 pip install python-magic
 
+export PATH="$HOME/.rbenv/bin:$PATH" 
+eval "$(rbenv init -)"
+
+rbenv install 2.5.6
+bundler install
+
+
 if lsb_release -d -s | grep -q 16.04; then
 	sudo apt-get install libsqlite3-mod-spatialite
 fi
